814269183b74e2feb54a1f33cc64c9df65f4103e
caitcroci <ccroci@email.sc.edu>
2024-02-22 14:18:04 -0500

    Fix #43

---
+++ degreesmart/Transcript.java
@@ -0,0 +1,41 @@
+package degreesmart;
+
+import java.util.ArrayList;
+
+public class Transcript {
+    private ArrayList<CompletedCourse> completedCourses;
+    private double gpa;
+    private double completedCreditHours;
+
+    public Transcript() {
+
+    }
+
+    public ArrayList<CompletedCourse>getCompletedCourses() {
+
+    }
+
+    public void addCompletedCourse(CompletedCourse course) {
+
+    }
+
+    public void removeCompletedCourse(CompletedCourse course) {
+
+    }
+
+    public double getGpa() {
+        return 0.0;
+    }
+
+    public double getCompletedCreditHours() {
+        return 0.0;
+    }
+
+    private void updateGpa() {
+
+    }
+
+    private void updateCompletedCreditHours() {
+
+    }
+}
