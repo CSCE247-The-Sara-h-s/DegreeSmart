75893033b52b67b4bcb5ddc0a17a028852f7a716
gw <gracewohlwinder@gmail.com>
2024-02-23 20:36:30 -0500

    Resolve #10

---
+++ degreesmart/CompletedCourse.java
@@ -0,0 +1,44 @@
+package degreesmart;
+
+public class CompletedCourse {
+	private Course course;
+	private Grade grade;
+	private Semester semester;
+	private int year;
+
+	public CompletedCourse(Course course, Grade grade, Semester semester, int year) {
+
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+
+	}
+
+	public Grade getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Grade grade) {
+
+	}
+
+	public Semester getSemester() {
+
+	}
+
+	public void setSemester(Semester semester) {
+
+	}
+
+	public int getYear() {
+
+	}
+
+	public void setYear(int year) {
+
+	}
+}
