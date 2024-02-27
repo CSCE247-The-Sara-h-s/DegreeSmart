8ddb9f41c4ae76e209b74bfbc31c123cd6e8006a
Sgclark20 <sgclark@email.sc.edu>
2024-02-27 13:27:09 -0500

    fixes 97

---
+++ degreesmart/DegreeSmartUI.java
@@ -0,0 +1,45 @@
+package degreesmart;
+
+import java.util.ArrayList;
+
+public class DegreeSmartUI {
+	private Application application;
+
+	public DegreeSmartUI() {
+		application = new Application();
+	}
+
+	public void run() {
+		ArrayList<Runnable> scenarios = new ArrayList<>(); // https://stackoverflow.com/a/67292304
+		scenarios.add(() -> scenario1());
+		scenarios.add(() -> scenario2());
+		scenarios.add(() -> scenario3());
+
+		for (int i = 0; i < scenarios.size(); i++) {
+			System.out.println("\n  ------- Scenario #" + (i + 1) + " ------- ");
+			try {
+				scenarios.get(i).run();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void scenario1() {
+
+	}
+
+	public void scenario2() {
+
+	}
+
+	public void scenario3() {
+
+	}
+
+	public static void main(String args[]) {
+		DegreeSmartUI degreeSmartInterface = new DegreeSmartUI();
+
+		degreeSmartInterface.run();
+	}
+}
