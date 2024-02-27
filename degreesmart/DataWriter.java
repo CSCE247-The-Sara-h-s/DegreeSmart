acb594c3cbde81e957eac243b5cadfff4cd2030a
Sarah Leaning <leaning.sarah@gmail.com>
2024-02-27 18:40:07 -0500

    Fix #102

--- degreesmart/DataWriter.java
+++ degreesmart/DataWriter.java
@@ -1,8 +1,27 @@
 package degreesmart;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class DataWriter extends DataConstants {
 	public static void saveUsers() {
+		UserList userList = UserList.getInstance();
+		ArrayList<User> users = userList.getUsers();
+		JSONArray jsonUsers = new JSONArray();
 
+		for(int i = 0; i < users.size(); i++) {
+			jsonUsers.add(users.get(i));
+		}
+
+		try (FileWriter file = new FileWriter(ADVISOR_FILE_NAME)) {
+			file.write(jsonUsers.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void saveCourses() {
