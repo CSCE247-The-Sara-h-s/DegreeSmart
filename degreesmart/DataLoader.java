95f3b5dd57743ba954ce400e47c86fce8e2acac8
caitcroci <ccroci@email.sc.edu>
2024-02-27 13:41:19 -0500

    Fix #98

--- degreesmart/DataLoader.java
+++ degreesmart/DataLoader.java
@@ -1,14 +1,49 @@
 package degreesmart;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class DataLoader extends DataConstants {
 	public static final ArrayList<User> getUsers() {
+		// go through each student and assign to advisor
+		// load advisor
+		// as you go through student, go to advisor class and add student to their list 
 		return new ArrayList<User>();
 	}
 
 	public static final ArrayList<Course> getCourses() {
-		return new ArrayList<Course>();
+				ArrayList<Course> course = new ArrayList<Course>();
+		
+		try {
+				// add file path
+			FileReader reader = new FileReader("course.json");
+			JSONParser parser = new JSONParser();
+			JSONArray coursesJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < coursesJSON.size(); i++) {
+				JSONObject classJSON = (JSONObject)coursesJSON.get(i);
+				String id = (String)classJSON.get(id);
+				String subject = (String)classJSON.get(subject);
+				String number = (String)classJSON.get(number);
+
+				//TODO: FIGURE OUT PRE-REQS
+				
+			}
+			
+			return course;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+		
+		//return new ArrayList<Course>();
 	}
 	
 	public static final ArrayList<RequirementSet> getRequirementSets() {
