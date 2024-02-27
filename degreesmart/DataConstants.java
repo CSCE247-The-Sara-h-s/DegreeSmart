328deeb9f607f5423b18560a9a3b2e3186a13b02
gw <gracewohlwinder@gmail.com>
2024-02-26 19:34:28 -0500

    Resolve #83

--- degreesmart/DataConstants.java
+++ degreesmart/DataConstants.java
@@ -1,3 +1,4 @@
+
 package degreesmart;
 
 public abstract class DataConstants {
@@ -7,6 +8,53 @@ public abstract class DataConstants {
 	protected static final String STUDENT_FILE_NAME = "json/student.json";
 	protected static final String COURSE_FILE_NAME = "json/course.json";
 	protected static final String REQUIREMENT_FILE_NAME = "json/requirement.json";
-	// TODO - add property constants
+	protected static final String USER_UUID = "uuid";
+	protected static final String USER_USERNAME = "username";
+	protected static final String USER_PASSWORD = "password";
+	protected static final String USER_FIRST_NAME = "first_name";
+	protected static final String USER_LAST_NAME = "last_name";
+	protected static final String USER_PREFERRED_NAME = "preferred_name";
+	protected static final String USER_EMAIL_ADDRESS = "email_address";
+	protected static final String ADVISOR_APPROVED = "approved";
+	protected static final String ADVISOR_ASSIGNED_STUDENTS = "assigned_students";
+	protected static final String PARENT_CHILDREN = "children";
+	protected static final String PARENT_PENDING_ACCESS_REQUESTS = "pending_access_requests";
+	protected static final String STUDENT_USC_ID = "usc_id";
+	protected static final String STUDENT_ADVISOR = "advisor";
+	protected static final String STUDENT_PARENTS = "parents";
+	protected static final String STUDENT_ADVISING_NOTES = "advising_notes";
+	protected static final String ADVISING_NOTE_NOTE = "note";
+	protected static final String ADVISING_NOTE_AUTHOR = "author";
+	protected static final String ADVISING_NOTE_TIME = "time";
+	protected static final String STUDENT_SCHOLARSHIPS = "scholarships";
+	protected static final String SCHOLARSHIP_NAME = "name";
+	protected static final String SCHOLARSHIP_MIN_GPA = "min_gpa";
+	protected static final String SCHOLARSHIP_MIN_CREDIT_HOURS = "min_credit_hours";
+	protected static final String STUDENT_GRADUATION_PLAN = "graduation_plan";
+	protected static final String GRADUATION_PLAN_REQUIREMENT_SETS = "requirement_sets";
+	protected static final String GRADUATION_PLAN_PREFERRED_GRADUATION_YEAR = "preferred_graduation_year";
+	protected static final String GRADUATION_PLAN_PREFERRED_GRADUATION_SEMESTER = "preferred_graduation_semester";
+	protected static final String GRADUATION_PLAN_MIN_CREDIT_HOURS = "preferred_min_credit_hours";
+	protected static final String GRADUATION_PLAN_MAX_CREDIT_HOURS = "preferred_max_credit_hours";
+	/******  FIXME  ******/
+	protected static final String GRADUATION_PLAN_SELECTED_COURSES = "selected_courses";
+	protected static final String GRADUATION_PLAN_SEMESTER_SCHEDULE = "semester_schedule";
+	/********************/
+	protected static final String COURSE_SET_UUID = "uuid";
+	protected static final String COURSE_SET_SUBJECT = "subject";
+	protected static final String COURSE_SET_NUMBER = "number";
+	protected static final String COURSE_SET_NAME = "name";
+	protected static final String COURSE_PREREQUISITES = "prerequisites";
+	protected static final String REQUIREMENT_NUM_CHOICES = "num_choices";
+	protected static final String COURSE_REQUIREMENT_MIN_GRADE = "min_grade";
+	protected static final String COURSE_REQUIREMENT_COURSE_OPTIONS = "course_options";
+	protected static final String NESTED_REQUIREMENT_REQUIREMENT_OPTIONS = "requirement_options";
+	protected static final String COURSE_COREQUISITES = "corequisites";
+	protected static final String COURSE_SEMESTERS_OFFERED = "semesters_offered";
+	protected static final String COURSE_CORE_CATEGORIES = "core_categories";
+	protected static final String COURSE_CREDIT_HOURS = "credit_hours";
+	protected static final String REQUIREMENT_SET_UUID = "uuid";
+	protected static final String REQUIREMENT_SET_NAME = "name";
+	protected static final String REQUIREMENT_SET_CATEGORY = "category";
+	protected static final String REQUIREMENT_SET_REQUIREMENTS = "requirements";
 }
-
