d130a7b6b282aed90faa1050a207065fa9ede95b
gw <gracewohlwinder@gmail.com>
2024-02-26 21:00:20 -0500

    Resolve #59

--- degreesmart/Transcript.java
+++ degreesmart/Transcript.java
@@ -3,39 +3,84 @@ package degreesmart;
 import java.util.ArrayList;
 
 public class Transcript {
+    private final double QUALITY_POINT_STEP = 0.5;
     private ArrayList<CompletedCourse> completedCourses;
     private double gpa;
     private double completedCreditHours;
 
     public Transcript() {
-
+        completedCourses = new ArrayList<CompletedCourse>();
+        gpa = 0.0;
+        completedCreditHours = 0.0;
     }
 
     public ArrayList<CompletedCourse> getCompletedCourses() {
         return completedCourses;
     }
 
-    public boolean addCompletedCourse(CompletedCourse course) {
-        return true;
+    public boolean addCompletedCourse(CompletedCourse completedCourse) {
+        if (completedCourses.add(completedCourse)) {
+            completedCreditHours += completedCourse.getCourse().getCreditHours();
+            updateGpa();
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean removeCompletedCourse(CompletedCourse course) {
-        return true;
+    public boolean removeCompletedCourse(CompletedCourse completedCourse) {
+        if (completedCourses.remove(completedCourse)) {
+            completedCreditHours -= completedCourse.getCourse().getCreditHours();
+            updateGpa();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public double getGpa() {
-        return 0.0;
+        return gpa;
     }
 
     public double getCompletedCreditHours() {
-        return 0.0;
+        return completedCreditHours;
     }
 
     private void updateGpa() {
+        double totalEarnedHours = 0.0;
 
-    }
+        for (CompletedCourse completedCourse : completedCourses) {
+            double courseEarnedHours = 0.0;
+            switch (completedCourse.getGrade()) {
+                case A:
+                    courseEarnedHours = 4.0;
+                    break;
+                case B_PLUS:
+                    courseEarnedHours = 3.5;
+                    break;
+                case B:
+                    courseEarnedHours = 3.0;
+                    break;
+                case C_PLUS:
+                    courseEarnedHours = 2.5;
+                    break;
+                case C:
+                    courseEarnedHours = 2.0;
+                    break;
+                case D_PLUS:
+                    courseEarnedHours = 1.5;
+                    break;
+                case D:
+                    courseEarnedHours = 1.0;
+                    break;
+                case F:
+                    courseEarnedHours = 0.0;
+                    break;
+            }
 
-    private void updateCompletedCreditHours() {
+            totalEarnedHours += courseEarnedHours * completedCourse.getCourse().getCreditHours();
+        }
 
+        gpa = totalEarnedHours / completedCreditHours;
     }
 }
