7cb0e5b875ce609c386b944289b87eb6996117f1
gw <gracewohlwinder@gmail.com>
2024-02-28 00:59:42 -0500

    Fix #121 - Add getName()

--- degreesmart/Course.java
+++ degreesmart/Course.java
@@ -42,6 +42,14 @@ public class Course {
 		this.subject = subject;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getNumber() {
 		return number;
 	}
