a650fade742df6ac7afe75d10582079b5812fbad
Sarah Leaning <leaning.sarah@gmail.com>
2024-03-02 15:02:45 -0500

    Fix #153

--- degreesmart/Administrator.java
+++ degreesmart/Administrator.java
@@ -7,5 +7,41 @@ public class Administrator extends User {
       String lastName) {
     super(uuid, username, password, email, firstName, lastName);
   }
+
+  public void approveAdvisor(Advisor advisor) {
+
+  }
+
+  public void addAssignedStudent(Advisor advisor, Student student) {
+
+  }
+
+  public void removeAssignedStudent(Advisor advisor, Student student) {
+
+  }
+
+  public void createCourse(Course course) {
+
+  }
+
+  public void deleteCourse(Course course) {
+
+  }
+
+  public void modifyCourse(Course modifiedCourse) {
+
+  }
+
+  public void createRequirementSet(RequirementSet requirementSet) {
+
+  }
+
+  public void deleteRequirementSet(RequirementSet requirementSet) {
+
+  }
+
+  public void modifyRequirementSet(RequirementSet modifiedSet) {
+    
+  }
 }
 
