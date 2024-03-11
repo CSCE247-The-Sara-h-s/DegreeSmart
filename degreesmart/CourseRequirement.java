b420a57d6a32fa0e3fa2d364f8b4fd5b55f838ec
gw <gracewohlwinder@gmail.com>
2024-03-10 21:01:55 -0400

    Resolve #160 - add toString(), equals()

--- degreesmart/CourseRequirement.java
+++ degreesmart/CourseRequirement.java
@@ -34,4 +34,31 @@ public class CourseRequirement extends Requirement {
 	public void setMinGrade(Grade minGrade) {
 		this.minGrade = minGrade;
 	}
+
+	public boolean equals(Object object) {
+		if (object == null || ! (object instanceof CourseRequirement)) {
+			return false;
+		}
+		CourseRequirement requirement = (CourseRequirement)object;
+
+		return minGrade.equals(requirement.getMinGrade())
+			&& getNumChoices() == requirement.getNumChoices()
+			&& courseOptions.size() == requirement.getCourseOptions().size()
+			&& courseOptions.containsAll(requirement.getCourseOptions())
+			&& requirement.getCourseOptions().containsAll(courseOptions);
+	}
+
+	public String toString() {
+		String str = minGrade + " or higher in ";
+
+		for (int i = 0; i < courseOptions.size(); i++) {
+			str += courseOptions.get(i).getSubject() + " " + courseOptions.get(i).getNumber();
+
+			if (i != courseOptions.size() - 1) {
+				str += " or ";
+			}
+		}
+
+		return str;
+	}
 }
