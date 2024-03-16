ca27a19cffa3fb17a01e1d8ff1ff4409255a34b7
gw <gracewohlwinder@gmail.com>
2024-03-15 23:48:24 -0400

    #228

--- degreesmart/DataConstants.java
+++ degreesmart/DataConstants.java
@@ -30,26 +30,25 @@ public abstract class DataConstants {
 	protected static final String SCHOLARSHIP_NAME = "name";
 	protected static final String SCHOLARSHIP_MIN_GPA = "min_gpa";
 	protected static final String SCHOLARSHIP_MIN_CREDIT_HOURS = "min_credit_hours";
+	/******  FIXME  ******/
 	protected static final String STUDENT_GRADUATION_PLAN = "graduation_plan";
 	protected static final String GRADUATION_PLAN_REQUIREMENT_SETS = "requirement_sets";
 	protected static final String GRADUATION_PLAN_PREFERRED_GRADUATION_YEAR = "preferred_graduation_year";
 	protected static final String GRADUATION_PLAN_PREFERRED_GRADUATION_SEMESTER = "preferred_graduation_semester";
 	protected static final String GRADUATION_PLAN_MIN_CREDIT_HOURS = "preferred_min_credit_hours";
 	protected static final String GRADUATION_PLAN_MAX_CREDIT_HOURS = "preferred_max_credit_hours";
-	/******  FIXME  ******/
 	protected static final String GRADUATION_PLAN_SELECTED_COURSES = "selected_courses";
 	protected static final String GRADUATION_PLAN_SEMESTER_SCHEDULE = "semester_schedule";
 	/********************/
+	protected static final String REQUIREMENT_CHOICES = "choices";
+	protected static final String REQUIREMENT_GRADE = "grade";
+	protected static final String REQUIREMENT_OPTIONS = "options";
 	protected static final String COURSE_UUID = "uuid";
 	protected static final String COURSE_SUBJECT = "subject";
 	protected static final String COURSE_NUMBER = "number";
 	protected static final String COURSE_NAME = "name";
 	protected static final String COURSE_DESCRIPTION = "description";
 	protected static final String COURSE_PREREQUISITES = "prerequisites";
-	protected static final String REQUIREMENT_NUM_CHOICES = "choices";
-	protected static final String COURSE_REQUIREMENT_MIN_GRADE = "min_grade";
-	protected static final String COURSE_REQUIREMENT_COURSE_OPTIONS = "course_options";
-	protected static final String NESTED_REQUIREMENT_REQUIREMENT_OPTIONS = "requirement_options";
 	protected static final String COURSE_COREQUISITES = "corequisites";
 	protected static final String COURSE_SEMESTERS_OFFERED = "semesters_offered";
 	protected static final String COURSE_CORE_CATEGORIES = "core_categories";
