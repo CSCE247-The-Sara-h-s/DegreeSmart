d249b5ba71a6660d2685c7df3b7e2963c413933b
Sarah Leaning <leaning.sarah@gmail.com>
2024-03-19 18:17:53 -0400

    Fix #268

---
+++ degreesmart/testing/UserTest.java
@@ -0,0 +1,12 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+//    private User user = new User();
+    
+}
