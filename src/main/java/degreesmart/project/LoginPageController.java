5197a937528347ab4916fe0b2ce635e0219a3946
gw <gracewohlwinder@gmail.com>
2024-04-07 14:28:30 -0400

    Resolve #367 - Add temporary user switch to Login Page

--- src/main/java/degreesmart/project/LoginPageController.java
+++ src/main/java/degreesmart/project/LoginPageController.java
@@ -2,8 +2,36 @@ package degreesmart.project;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 
 public class LoginPageController {
-
+	@FXML
+	private TextField usernameField;
     
+    @FXML
+    private void DEV_test_login(KeyEvent event) {
+    	if (event.getCode() != KeyCode.ENTER) {
+    		return;
+    	}
+
+    	try {
+    		switch (usernameField.getText().toLowerCase()) {
+	    	case "admin":
+	    		App.setRoot("adminHome");
+	    		break;
+	    	case "advisor":
+	    		App.setRoot("loginpage");
+	    		break;
+	    	case "student":
+	    		App.setRoot("student-graduation-plan");
+	    		break;
+	    	case "parent":
+	    		App.setRoot("loginpage");
+	    		break;
+	    	}
+    	} catch (Exception e) {
+    	}
+    }
 }
