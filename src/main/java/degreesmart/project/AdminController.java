30337574ec3c11867b4f01e15c4799aa12e58061
Sarah Leaning <leaning.sarah@gmail.com>
2024-04-11 13:33:23 -0400

    Fix #346

---
+++ src/main/java/degreesmart/project/AdminController.java
@@ -0,0 +1,93 @@
+package degreesmart.project;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import javafx.event.ActionEvent;
+
+public class AdminController implements Initializable {
+    @FXML
+    private HeaderPaneController headerPaneController;
+
+    @SuppressWarnings("unchecked")
+    public void initialize(URL url, ResourceBundle rb) {
+        headerPaneController.getComboBox().getItems().addAll(
+            "Log Out"
+        );
+
+        headerPaneController.getComboBox().setValue("Hello <user>!");
+
+        headerPaneController.getComboBox().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    switch(headerPaneController.getComboBox().getValue().toString().toLowerCase()) {
+                        case "log out":
+                            App.setRoot("loginpage");
+                            break;
+                    }
+                } catch (Exception e) {
+                }
+            }
+        });
+        
+        Label studentManagement = new Label("Student Management");
+        Label courseManagement = new Label("Course Management");
+        Label advisorManagement = new Label("Advisor Management");
+
+        studentManagement.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    App.setRoot("student-management");
+                } catch (Exception e) {
+                }
+            }
+        });
+
+        courseManagement.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    App.setRoot("course-management");
+                } catch (Exception e) {
+                }
+            }
+        });
+
+        advisorManagement.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    App.setRoot("advisor-management");
+                } catch (Exception e) {
+                }
+            }
+        });
+
+        HBox.setHgrow(studentManagement, Priority.ALWAYS);
+        HBox.setHgrow(courseManagement, Priority.ALWAYS);
+        HBox.setHgrow(advisorManagement, Priority.ALWAYS);
+
+        studentManagement.setMaxWidth(Double.MAX_VALUE);
+        courseManagement.setMaxWidth(Double.MAX_VALUE);
+        advisorManagement.setMaxWidth(Double.MAX_VALUE);
+
+        studentManagement.setAlignment(Pos.CENTER);
+        courseManagement.setAlignment(Pos.CENTER);
+        advisorManagement.setAlignment(Pos.CENTER);
+
+        headerPaneController.getLinks().getChildren().add(studentManagement);
+        headerPaneController.getLinks().getChildren().add(courseManagement);
+        headerPaneController.getLinks().getChildren().add(advisorManagement);
+    }
+}
--- src/main/java/degreesmart/project/AdminHomeController.java
+++ src/main/java/degreesmart/project/AdminHomeController.java
@@ -1,8 +1,29 @@
 package degreesmart.project;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
-public class AdminHomeController {
-    
+public class AdminHomeController extends AdminController implements Initializable {
+    @FXML
+    private GridPane background;
+
+    @FXML
+    private HeaderPaneController headerPaneController;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        super.initialize(url, rb);
+        background.setStyle("-fx-background-image: url(\"" 
+            + App.class.getResource("/images/background.jpg").toExternalForm() +"\");" +
+            "-fx-background-size: cover;" +
+            "-fx-background-position: center;");
+
+        headerPaneController.getPageTitle().setText("Welcome");
+
+
+    }
 }
