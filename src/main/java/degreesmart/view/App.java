8576e766921b51541e60ee90269e9283b755c976
Sgclark20 <sgclark@email.sc.edu>
2024-04-14 18:51:33 -0400

    fixes #379

--- src/main/java/degreesmart/view/App.java
+++ src/main/java/degreesmart/view/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
         UserList.getInstance();
         stage.setTitle("DegreeSmart");
         stage.getIcons().add(new Image(App.class.getResourceAsStream("/images/icon.png")));
-        scene = new Scene(loadFXML("loginpage"), 640, 480);
+        scene = new Scene(loadFXML("loginpage"), 710, 675);
         stage.setScene(scene);
         stage.show();
     }
