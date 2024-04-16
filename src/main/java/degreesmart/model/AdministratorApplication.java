d774ca6394d11cda81c924fdae3f4671049c62ce
gw <gracewohlwinder@gmail.com>
2024-04-16 16:47:02 -0400

    Resolve #396 - Create AdministrorApplication

---
+++ src/main/java/degreesmart/model/AdministratorApplication.java
@@ -0,0 +1,60 @@
+package degreesmart.model;
+
+import java.util.ArrayList;
+
+public class AdministratorApplication extends Application {
+	private static AdministratorApplication application;
+	private Administrator activeUser;
+
+	private AdministratorApplication() {
+		activeUser = (Administrator) Application.getInstance().getActiveUser();
+	}
+
+	public static AdministratorApplication getInstance() throws IllegalArgumentException {
+		Role role = Application.getInstance().getActiveUser().getRole();
+
+		if (role != Role.ADMINISTRATOR) {
+			throw new IllegalArgumentException("Logged in user must have '" + role + "' role");
+		}
+
+		if (application == null) {
+			application = new AdministratorApplication();
+		}
+		
+		return application;
+	}
+
+	public Administrator getActiveUser() {
+		return activeUser;
+	}
+
+	public void logOut() {
+		activeUser = null;
+		application = null;
+		super.logOut();
+	}
+
+	public boolean deleteAccount(User user) {
+        return UserList.getInstance().deleteUser(user);
+    }
+
+    public ArrayList<User> getUsers() {
+    	return UserList.getInstance().getUsers();
+    }
+
+    public ArrayList<User> getUsers(String firstName, String lastName) {
+        return UserList.getInstance().getUsers(firstName, lastName);
+    }
+
+    public User getUser(String username) {
+    	return UserList.getInstance().getUser(username);
+    }
+
+    public ArrayList<Student> getStudents() {
+    	return UserList.getInstance().getStudents();
+    }
+
+    public Student getStudent(String uscId) {
+    	return UserList.getInstance().getStudent(uscId);
+    }
+}
