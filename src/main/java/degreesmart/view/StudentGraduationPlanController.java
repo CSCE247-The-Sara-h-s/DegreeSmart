910b92926ab2056f898d9129ed818c7a52e7270b
gw <gracewohlwinder@gmail.com>
2024-04-19 23:22:40 -0400

    Resolve #448 - Add current and planned courses to GraduationPlan; edit style

--- src/main/java/degreesmart/view/StudentGraduationPlanController.java
+++ src/main/java/degreesmart/view/StudentGraduationPlanController.java
@@ -74,9 +74,11 @@ public class StudentGraduationPlanController extends StudentController implement
         creditsEarned.setText("" + application.getCreditHours());
 
         addCompletedSemesters(application.getCompletedCourses());
+        addPlannedSemesters(application.getCurrentCourses());
+        addPlannedSemesters(application.getPlannedCourses());
     }
 
-    public VBox getSemesterVBox() {
+    private VBox getSemesterVBox() {
         VBox semester = null;
 
         try {
@@ -85,6 +87,7 @@ public class StudentGraduationPlanController extends StudentController implement
             fxmlLoader.setLocation(App.class.getResource("/library/graduation-plan-semester.fxml"));
             semester = fxmlLoader.<VBox>load();
             GridPane details = (GridPane)semester.lookup("#semesterDetails");
+            HBox spacer = (HBox) semester.lookup("#spacer");
 
             ((ImageView)semester.lookup("#expandSemester")).setOnMousePressed(new EventHandler<MouseEvent>() {
                 @Override
@@ -92,15 +95,21 @@ public class StudentGraduationPlanController extends StudentController implement
                     if (details.isVisible()) {
                         details.setVisible(false);
                         details.setManaged(false);
+                        spacer.setVisible(false);
+                        spacer.setManaged(false);
                     } else {
                         details.setVisible(true);
                         details.setManaged(true);
+                        spacer.setVisible(true);
+                        spacer.setManaged(true);
                     }
                 }
             });
 
             details.setVisible(false);
             details.setManaged(false);
+            spacer.setVisible(false);
+            spacer.setManaged(false);
             semesterList.getChildren().add(semester);
         } catch (Exception e) {
             e.printStackTrace();
@@ -137,7 +146,79 @@ public class StudentGraduationPlanController extends StudentController implement
                 new Label(course.getShortName()),
                 new Label(course.getName()),
                 new Label("" + course.getCreditHours()),
-                new Label(Grade.B.toString()),
+                new Label(completedCourse.getGrade().toString()),
+                new Label("Computer Science - Major")
+            );
+
+            EventHandler<MouseEvent> clickEvent = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    System.out.println(course);
+                }
+            };
+
+            EventHandler<MouseEvent> enterEvent = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    for (Label label : labels) {
+                        label.setStyle("-fx-padding: 3 5; -fx-background-color: rgba(101, 8, 8, 0.2);");
+                    }
+                }
+            };
+
+            EventHandler<MouseEvent> exitEvent = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    for (Label label : labels) {
+                        label.setStyle("-fx-padding: 3 5; -fx-background-color: transparent;");
+                    }
+                }
+            };
+
+            for (Label label : labels) {
+                label.setOnMouseClicked(clickEvent);
+                GridPane.setRowIndex(label, row);
+                GridPane.setColumnIndex(label, col++);
+                details.getChildren().add(label);
+
+                label.setStyle("-fx-padding: 3 5;");
+                label.setMaxHeight(Double.MAX_VALUE);
+                label.setMaxWidth(Double.MAX_VALUE);
+                label.setOnMouseEntered(enterEvent);
+                label.setOnMouseExited(exitEvent);
+            }
+        }
+    }
+
+    private void addPlannedSemesters(ArrayList<PlannedCourse> courses) {
+        Collections.sort(courses, Collections.reverseOrder());
+
+        String blockLabel = "";
+        VBox semester = null;
+        GridPane details = null;
+        int row = 0;
+        int col = 0;
+
+        for (PlannedCourse plannedCourse : courses) {
+            if (!plannedCourse.getTerm().toString().equals(blockLabel)) {
+                semester = getSemesterVBox();
+                details = (GridPane) semester.lookup("#semesterDetails");
+                blockLabel = plannedCourse.getTerm().toString();
+                ((Label) semester.lookup("#semesterName")).setText(blockLabel);
+                row = 0;
+            }
+            col = 0;
+            row++;
+            Course course = plannedCourse.getCourse();
+
+
+            ArrayList<Label> labels = new ArrayList<Label>();
+            Collections.addAll(
+                labels,
+                new Label(course.getShortName()),
+                new Label(course.getName()),
+                new Label("" + course.getCreditHours()),
+                new Label(Grade.D.toString()),
                 new Label("Computer Science - Major")
             );
 
