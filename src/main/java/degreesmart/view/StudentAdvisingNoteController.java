72854bddacbd71dcd3f7e015998bea4d70401a59
Sgclark20 <sgclark@email.sc.edu>
2024-04-21 14:10:07 -0400

    fixes #355

--- src/main/java/degreesmart/view/StudentAdvisingNoteController.java
+++ src/main/java/degreesmart/view/StudentAdvisingNoteController.java
@@ -1,25 +1,60 @@
 package degreesmart.view;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import degreesmart.model.AdvisingNote;
+import degreesmart.model.Application;
+import degreesmart.model.StudentApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.control.Label;
-import javafx.geometry.Pos;
-import java.lang.reflect.*;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
 public class StudentAdvisingNoteController extends StudentController implements Initializable {
+
     @FXML
     private HeaderPaneController headerPaneController;
 
+    @FXML
+    private TableView<AdvisingNote> userTableView;
+
+    @FXML
+    private TextField searchField;
+
+
+    @SuppressWarnings("unchecked")
     public void initialize(URL url, ResourceBundle rb) {
+       
         super.initialize(url, rb);
-        headerPaneController.getPageTitle().setText("Advising Notes");
+        StudentApplication application = StudentApplication.getInstance();
+        String title = Application.getInstance().getFirstName() + "'s Advising Notes";
+        headerPaneController.getPageTitle().setText(title);
+
+
+
+        // Setup TableView columns
+        TableColumn<AdvisingNote, String> dateColumn = new TableColumn<>("Date");
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        TableColumn<AdvisingNote, String> advisorNameColumn = new TableColumn<>("Advisor Name");
+        advisorNameColumn.setCellValueFactory(new PropertyValueFactory<>("advisorName"));
+
+        TableColumn<AdvisingNote, String> advisingNotesColumn = new TableColumn<>("Advising Notes");
+        advisingNotesColumn.setCellValueFactory(new PropertyValueFactory<>("advisingNotes"));
+
+        userTableView.getColumns().addAll(dateColumn, advisorNameColumn, advisingNotesColumn);
+
+        searchField.setOnKeyPressed(event -> searchUsers());
+
+    }
+
+    private void searchUsers() {String searchText = searchField.getText().toLowerCase().trim();
+
     }
 }
